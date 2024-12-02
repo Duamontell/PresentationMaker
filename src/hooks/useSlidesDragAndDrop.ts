69c12e4f00e96cf137d5dc04b69fdb187eaef630
Maksim <duamontell@gmail.com>
2024-12-02 19:26:32 +0300

    Little changes

--- src/hooks/useSlidesDragAndDrop.ts
+++ src/hooks/useSlidesDragAndDrop.ts
@@ -9,20 +9,18 @@ export function useSlidesDragAndDrop(slides: Slide[]) {
 	function onDragStart(event: React.DragEvent, slideId: string) {
 		setDraggedSlideId(slideId);
 		event.dataTransfer.effectAllowed = 'move';
-		console.log("onDragStart");
 	}
 
 	function onDragOver(event: React.DragEvent) {
-		event.preventDefault(); // Разрешает перетаскивание
+		event.preventDefault();
 	}
 
 	function onDrop(_: React.DragEvent, targetSlideId: string) {
-		// if (!draggedSlideId) return;
 
 		const draggedIndex = slides.findIndex((slide) => slide.id === draggedSlideId);
 		const targetIndex = slides.findIndex((slide) => slide.id === targetSlideId);
 
-		if (draggedIndex !== -1 && targetIndex !== -1 && draggedIndex !== targetIndex) {
+		if (draggedIndex !== targetIndex) {
 			const updatedSlides = [...slides];
 			const [draggedSlide] = updatedSlides.splice(draggedIndex, 1);
 			updatedSlides.splice(targetIndex, 0, draggedSlide);
